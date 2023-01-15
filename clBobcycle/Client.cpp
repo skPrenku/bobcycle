7b98abece302df1549dd61844fdd55d9dac20f00
skPrenku <iliri@iliri.net>
2023-01-15 01:00:40 +0100

    client structor added, init machine specs at startup

---
+++ clBobcycle/Client.cpp
@@ -0,0 +1,38 @@
+#include "Client.h"
+#include <WinSock2.h>
+#include "usb.h"
+#include "network.h"
+
+
+
+
+
+Client::Client() {
+
+    const DWORD usrNameSize = 32;
+    const DWORD pcNameSize = 32;
+    char usrName[usrNameSize];
+    char pcName[pcNameSize];
+
+    bool iRes = false;
+
+    iRes = GetComputerNameA((LPSTR)&pcName, (LPDWORD)&pcNameSize);
+    if (iRes)
+    {
+        computerName = pcName;
+    }
+        //  printf("Error getting PC name");
+
+    iRes = GetUserNameA((LPSTR)&usrName, (LPDWORD)&usrNameSize);
+  
+    if (iRes)
+    {
+        userName = usrName;
+    }
+
+
+     disks = displayDisks().c_str();
+     ip = getIPv4("").c_str();
+
+
+}
---
+++ clBobcycle/Client.h
@@ -0,0 +1,13 @@
+#pragma once
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+struct Client {
+
+	Client();
+
+	std::string computerName ;
+	std::string userName;
+	std::string ip;
+	std::string disks;
+};
