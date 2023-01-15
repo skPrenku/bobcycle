dfaefd07aa40387310485d5fbaf2c37202bb3c85
skPrenku <iliri@iliri.net>
2023-01-15 01:03:25 +0100

    added string stream to store all disks infos

--- clBobcycle/usb.cpp
+++ clBobcycle/usb.cpp
@@ -1,6 +1,7 @@
 #include "usb.h"
 #include <iomanip>
 #include <Windows.h>
+#include <sstream>
 
 
 disks onlineDisk[6];
@@ -68,26 +69,31 @@ bool listDrives(int& totalDisks)
 
 
 
-void displayDisks() {
+std::string displayDisks() {
 
 	int activeDisks;
-
+	std::stringstream ss;
 	if (listDrives(activeDisks) == 0)
 	{
 		printf("Reading Drives failed, ERROR CODE: %d", GetLastError());
 	}
 	else {
-		printf("Total Drives found: %d \n\n", activeDisks);
+		
+
+		ss << "Total Drives found: %d", activeDisks , "\n";
 
 		for (int i = 0; i < activeDisks; i++)
 		{
 
+			
 
+			//std::cout << "Disk: " << onlineDisk[i].diskName.c_str() << std::endl;
+			ss << "Disk: " << onlineDisk[i].diskName.c_str() << "\n";
+			ss << "Disk Total Space: " << std::fixed << std::setprecision(2) << double(onlineDisk[i].totalBytes) / 1073741824 << " GB\n";
+			ss << "Disk Free Space: " << std::fixed << std::setprecision(2) << double(onlineDisk[i].totalFreeBytes) / 1073741824 << " GB\n\n";
 
-			std::cout << "Disk: " << onlineDisk[i].diskName.c_str() << std::endl;
-
-			std::cout << "Disk Space: " << std::fixed << std::setprecision(2) << double(onlineDisk[i].totalBytes) / 1073741824 << " GB" << std::endl;
-			std::cout << "Disk Space: " << std::fixed << std::setprecision(2) << double(onlineDisk[i].totalFreeBytes) / 1073741824 << " GB" << std::endl;
+			//std::cout << "Disk Total Space: " << std::fixed << std::setprecision(2) << double(onlineDisk[i].totalBytes) / 1073741824 << " GB" << std::endl;
+			//std::cout << "Disk Free Space: " << std::fixed << std::setprecision(2) << double(onlineDisk[i].totalFreeBytes) / 1073741824 << " GB" << std::endl;
 			/* devide by 1073741824 to get the right Disk-Space on windows*/
 
 
@@ -95,4 +101,5 @@ void displayDisks() {
 		}
 	}
 
+	return ss.str();
 }
